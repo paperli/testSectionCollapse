7eebe8d4fd20725dc715956721098f557e4489a9
Paper <paper.li@gmail.com>
2015-12-25 09:34:37 +0800

    Initial Commit

---
+++ testSectionTests/testSectionTests.swift
@@ -0,0 +1,36 @@
+//
+//  testSectionTests.swift
+//  testSectionTests
+//
+//  Created by paper on 2015/12/25.
+//  Copyright © 2015年 paper. All rights reserved.
+//
+
+import XCTest
+@testable import testSection
+
+class testSectionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
